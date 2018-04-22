62516c11085da50a84434468d8e7057228118f14
VuNhat Tan <vunhattan84@gmail.com>
2018-04-23 00:00:30 +0900

    add componentscan and remove spring.factories setting

--- livelessons-bootstrap/livelessons-bootstrap-autoconfiguration/src/main/java/demo/BootstrapAutoConfigureApplication.java
+++ livelessons-bootstrap/livelessons-bootstrap-autoconfiguration/src/main/java/demo/BootstrapAutoConfigureApplication.java
@@ -2,10 +2,12 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
+@ComponentScan
 public class BootstrapAutoConfigureApplication {
 
 	public static void main(String[] args) {
