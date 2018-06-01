db317cb351f97925159593d74efe52dc865e7c2f
Vu, Tan | Sam | ECFD <tan.vu@rakuten.com>
2018-06-02 00:30:07 +0900

    change to Spring boot 2.0.2 and add using mysql

--- livelessons-data/livelessons-data-flyway/src/main/java/demo/Car.java
+++ livelessons-data/livelessons-data-flyway/src/main/java/demo/Car.java
@@ -2,13 +2,14 @@ package demo;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Car {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
 	private String make;
